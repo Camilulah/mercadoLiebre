12b7adbcf05281d9283b914da00295b61cbd2868
camilavd <camilavd045@gmail.com>
2023-11-23 13:01:05 -0300

    a ver

--- src/app.js
+++ src/app.js
@@ -12,3 +12,9 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"))
 })
+app.get("/register", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+})
+app.get("/login", (req,res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+})
